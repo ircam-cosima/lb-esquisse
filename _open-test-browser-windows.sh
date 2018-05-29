a9e466870ee579fe0faabfc312fd0a87ed881777
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2018-05-29 18:36:57 +0200

    removed file

--- _open-test-browser-windows.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MAX=1;
-URL=www.google.com;
-
-for (( i=0; i<=$MAX; i+=1 )); do
-    echo "$i";
-    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --incognito --new-window "$URL";
-done;
